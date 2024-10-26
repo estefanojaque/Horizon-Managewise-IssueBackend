9b3b6018df36c931d821ba000af227e405447756
estefanojaque <u202225466@upc.edu.pe>
2024-10-26 06:57:54 -0500

    feature: Add package shared

--- issue-platform/issue-platform/src/main/java/pe/edu/upc/managewise/issue_platform/IssuePlatformApplication.java
+++ issue-platform/issue-platform/src/main/java/pe/edu/upc/managewise/issue_platform/IssuePlatformApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.managewise.issue_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IssuePlatformApplication {
 
 	public static void main(String[] args) {
---
+++ issue-platform/issue-platform/src/main/java/pe/edu/upc/managewise/issue_platform/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.managewise.issue_platform.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
