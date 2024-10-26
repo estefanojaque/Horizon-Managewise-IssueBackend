69c1ed8899f7302346545a3a694cac8a118b9d02
estefanojaque <u202225466@upc.edu.pe>
2024-10-26 05:27:17 -0500

    My first commit of my backend

---
+++ issue-platform/issue-platform/src/main/java/pe/edu/upc/managewise/issue_platform/IssuePlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.managewise.issue_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuePlatformApplication.class, args);
+	}
+
+}
---
+++ issue-platform/issue-platform/src/test/java/pe/edu/upc/managewise/issue_platform/IssuePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.managewise.issue_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
